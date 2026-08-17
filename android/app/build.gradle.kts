b0730b561a2db6df6bfd85acfcd697e333fc7f45
a77atm <anamido76@gmail.com>
2026-08-17 11:28:38 +0000

    Actually apply google-services plugin, applicationId fix, and INTERNET permission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.shoe_store"
+        applicationId = "shoesapp.com"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
